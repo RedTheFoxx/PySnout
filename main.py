05b17939949d99e9a004e04e64dda09fa913aa1d
RedTheFoxx <morisseau.jeremy@gmail.com>
2023-03-05 19:39:17 -0800

    main file added

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-# Future bot : https://red-space.notion.site/PySnout-fc49399977a44d0685e25bc4be97e5f5
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.messages = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+TOKEN = "MTA1MDU4NTA4ODI2MzQ2Mjk2NA.G1VIM_.zM24VrWVwjOng10o2D_cxaa6-hP94jzZTSsPG0"
+
+
+@bot.event
+async def on_ready():
+    print("PySnout est en ligne !")
+
+
+bot.run(TOKEN)
