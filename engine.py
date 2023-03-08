550fe55230ad69cec67869708dcb3647a50c1bef
RedTheFoxx <morisseau.jeremy@gmail.com>
2023-03-08 16:12:50 +0100

    !play local file implementation

--- engine.py
+++ engine.py
@@ -1,1 +1,16 @@
-# Basic bot functions here
+from discord import FFmpegPCMAudio
+
+
+async def play_audio(ctx, file_path):
+    if not ctx.author.voice:
+        await ctx.send("Vous devez être connecté dans un canal vocal pour utiliser cette commande !")
+        return
+
+    vc = ctx.author.voice.channel
+
+    if ctx.voice_client is None:
+        await vc.connect()
+
+    source = FFmpegPCMAudio(file_path)
+
+    ctx.voice_client.play(source)
--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+# Required params :
+# ffmpeg + PATH + ffmpeg.exe
+# PyNaCl (for audio capablities)
+# youtube-dl or yt-dlp (for audio download)
+
+# TODO : add a command to stop the audio
+# TODO : download audio via !play command
+# TODO : display audio preparation progress in current channel
+
 import discord
 from discord.ext import commands
+
+from engine import play_audio
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix="!", intents=intents)
@@ -16,5 +27,7 @@ async def on_ready():
 @bot.command("play")
 async def play(ctx):
     await ctx.send("Bientôt, je diffuserais de l'audio ! :musical_note:")
+    await play_audio(ctx, 'audio_bucket/foreigner_demo.mp3')
 
 bot.run(TOKEN)
+
