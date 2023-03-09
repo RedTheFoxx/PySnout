274213564e1dbe5c0b1038a83c8b59e1086ca779
RedTheFoxx <morisseau.jeremy@gmail.com>
2023-03-09 02:19:31 +0100

    added QoL embed notifications

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 TOKEN = "..."
 
+# Globals
+player_is_playing = False
+
 
 @bot.event
 async def on_ready():
@@ -23,15 +26,36 @@ async def on_ready():
 
 
 @bot.command("play")
-async def play(ctx, url):
+async def play(ctx, url=None):
+    if url is None:
+        embed_no_url = discord.Embed(title="Erreur : URL non renseignée !", color=0xe04f5f)
+        embed_no_url.set_author(name="Lecteur", icon_url="https://cdn-icons-png.flaticon.com/512/463/463612.png")
+        embed_no_url.set_footer(text="PySnout - P1")
+        await ctx.send(embed=embed_no_url)
+        return
     if not url.startswith('https://www.youtube.com/watch?v='):
-        await ctx.send('Mauvais format d\'URL !')
+        embed_bad_format = discord.Embed(title="Erreur : L'URL n'est pas du bon format (YouTube uniquement)", color=0xe04f5f)
+        embed_bad_format.set_author(name="Lecteur", icon_url="https://cdn-icons-png.flaticon.com/512/463/463612.png")
+        embed_bad_format.set_footer(text="PySnout - P1")
+        await ctx.send(embed=embed_bad_format)
         return
 
-    await ctx.send("Chargement ...")
+    embed_download = discord.Embed(title="Téléchargement ...", color=0x3b77b1)
+    embed_download.set_author(name="Lecteur", icon_url="https://cdn-icons-png.flaticon.com/512/6995/6995413.png")
+    embed_download.set_footer(text="PySnout - P1")
+
+    await ctx.send(embed=embed_download)
+
     pathtoplay = await download_audio(url)
-    await ctx.send("Terminé. Lecture en cours ...")
+
+    embed_playing = discord.Embed(title="Lecture en cours", color=0x4eb03b)
+    embed_playing.set_author(name="Lecteur", icon_url="https://cdn-icons-png.flaticon.com/512/7826/7826802.png")
+    embed_playing.set_footer(text="PySnout - P1")
+    await ctx.send(embed=embed_playing)
+
     await play_audio(ctx, pathtoplay)
 
+    global player_is_playing
+    player_is_playing = True
 
 bot.run(TOKEN)
